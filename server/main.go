4ef11ef8ce9df5adf92be12369246eb7e04fdc40
Sean Corbett <sean@tomis.tech>
2018-07-21 14:47:03 -0600

    remove .env load from main.go

--- server/main.go
+++ server/main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"github.com/joho/godotenv"
-	"log"
-)
+// import (
+// 	"github.com/joho/godotenv"
+// 	"log"
+// )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	StartServer()
 
